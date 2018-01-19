bdada71a2e5e0d20e3fa413e8039112a96d55a2c
timmysong <tinysong1226@gmail.com>
2018-01-19 17:45:13 +0800

    update prometheus config

--- monitoring/prometheus/v1.6.1/package.sh
+++ monitoring/prometheus/v1.6.1/package.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 VERSION="tce-2.8"
-tar -czvf prometheus-demonSet-$VERSION.tar.gz ./prometheus-configmap.yaml ./prometheus-daemonSet.yaml ./prometheus-rbac-auth.yaml ./prometheus-config-rule.yaml ./prometheus-service.yaml ./haproxy-exporter.yaml
+tar -czvf prometheus-demonSet-$VERSION.tar.gz ./prometheus-configmap.yaml ./prometheus-daemonSet.yaml ./prometheus-rbac-auth.yaml ./prometheus-config-rule.yaml ./prometheus-service.yaml ./haproxy-exporter.yaml ./prometheus-node-exporter.yaml
